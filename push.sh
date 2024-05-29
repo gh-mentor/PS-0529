a43d012ae24c327dbdb159d9d1af2ebae2cb5892
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-05-29 17:07:25 -0400

    Update

---
+++ push.sh
@@ -0,0 +1,13 @@
+# This bash script uses 'git' to do the following:
+# 1)  stage changes
+# 2) commit changes
+# 3) push changes to the upstream  repository on branch 'main'.
+
+# Stage changes
+git add .
+
+# Commit changes
+git commit -m "Update"
+
+# Push changes to the upstream repository on branch 'main'
+git push origin main
